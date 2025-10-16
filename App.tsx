9533371393ed086cb28680f2b5447883ca3bec78
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-10-16 14:48:22 +0200

    Replaced the code withe new more correct code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [output, setOutput] = useState<string[]>([]);
+  const numbers: number[] = [2, 4, 6, 8, 10];
+
+  const runFor = (): void => {
+    const Lines: string[] = [];
+    for (let i = 0; i < numbers.length; i++) {
+      Lines.push(`for: i=${i} -> ${numbers[i]}`);
+    }
+    setOutput(Lines);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>🔁Loop Di Loop</Text>
+
+      <View>
+        <Button title="Start Loop" onPress={runFor} />
+
+        {output.length === 0 ? (
+          <Text>Output will appear over here</Text>
+        ) : (
+          output.map((line, i) => <Text key={i}>{line}</Text>)
+        )}
+      </View>
     </View>
   );
 }
@@ -15,6 +35,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 15,
+  },
+  heading: {
+    fontSize: 65,
+    fontWeight: 'bold',
   },
 });
